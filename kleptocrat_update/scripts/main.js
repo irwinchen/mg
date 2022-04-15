f5aef12a733dfd23e18df06ec437ead47b013d20
Irwin Chen <cheni@newschool.edu>
2022-04-15 16:36:15 -0400

    New working js framework

--- kleptocrat_update/scripts/main.js
+++ kleptocrat_update/scripts/main.js
@@ -39,3 +39,23 @@ gsap.from(".p2", {
     opacity: 0,
     duration: 3
 });
+
+let card_el = document.getElementById("CardModal");
+let card_h3 = document.querySelector(".card__header>h3");
+let card_h4 = document.querySelector(".card__body>h4");
+let card_p = document.querySelector(".card__body>p");
+let card_front = document.querySelector(".card__face--front");
+let card_back = document.querySelector(".card__face--back");
+
+
+let btns = document.querySelectorAll('button');
+
+for (i of btns) {
+  i.addEventListener('click', function() {
+    card_front.style["background-image"]="url('images/" + this.getAttribute("data-front") + "')";
+    card_h3.innerHTML = this.getAttribute("data-h3");
+    card_h4.innerHTML = this.getAttribute("data-h4");
+    card_p.innerHTML = this.getAttribute("data-p");
+    card_back.style["background-color"]="var(--" + this.getAttribute("data-h3").toLowerCase()+")";
+  });
+}
