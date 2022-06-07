789d1747f5fa277d0b8e0579e12602594a1ed833
Irwin Chen <cheni@newschool.edu>
2022-06-07 07:26:11 -0400

    Updated data and js
    - differentiated former and current employees, so can we make formers green, current Citi employees red, and all others yellow.
    - hover shows connected nodes (removed tooltip for now)
    - updated data

--- citimadoff/radial.js
+++ citimadoff/radial.js
@@ -1,10 +1,23 @@
-var pointScale = d3.scaleOrdinal()
+var colorScale = d3.scaleOrdinal()
   .domain(["Person", "Entity", "Division", "Fund"])
   .range(["#1f77b4","#ff7f0e","#2ca02c","#d62728"]);
+
+var triangleU = d3.symbol().type(d3.symbolTriangle)(),
+circle = d3.symbol().type(d3.symbolCircle)(),
+cross = d3.symbol().type(d3.symbolCross)(),
+square = d3.symbol().type(d3.symbolSquare)();
 
 var symbols = d3.scaleOrdinal()
 .domain(["Person", "Entity", "Division", "Fund"])
 .range([d3.symbolCircle, d3.symbolSquare, d3.symbolCross, d3.symbolTriangle]);
+
+var symbolScale =  d3.scaleOrdinal()
+  .domain(["Person", "Entity", "Division", "Fund"])
+  .range([ circle, square, cross, triangleU] );
+
+
+let zoom = d3.zoom()
+  .on('zoom', handleZoom);
 
 var svg = d3.select("svg"),
 width = +svg.attr("width"),
@@ -44,19 +57,24 @@ var atty = d3.select(".box").append("text")
     .attr("fill", "red")
     .text("ATTORNEY WORK PRODUCT");
 
-d3.json("newdata.json").then(function(graph) {
+// d3.json("newdata.json").then(function(graph) {
+d3.json("data_jun7.json").then(function(graph) {
     // if (error) throw error;
+    // set the nodes
+    var nodes = graph.nodes;
+    // links between nodes
+    var links = graph.links;
 
-    var link = d3.select("svg")
+    var link = d3.select("svg g")
     .append("g")
     .attr("class", "links")
     .selectAll("line")
     .data(graph.links)
     .enter().append("line")
-    .attr("stroke", "#aaa")
+    .attr("stroke", "#333")
     .attr("stroke-width", 1);
 
-    var node = d3.select("svg")
+    var node = d3.select("svg g")
     .append("g")
     .attr("class", "nodes")
     .selectAll("g")
@@ -65,9 +83,17 @@ d3.json("newdata.json").then(function(graph) {
 
     var shapes = node.append("g")
         .append("path")
-        .attr("fill", function(d) { return pointScale(d.type); })
+        .attr("fill", function(d) { 
+            if (d.status == "Current") {
+                return "#DC322F";
+            } else if (d.status == "Former") {
+                return "#00AB93";
+            } else {
+                return "#DFA91E";
+            }
+        })
         // .attr("d", d3.symbol().type(d3.symbolSquare))
-        .attr("d", d3.symbol().type(function(d) { return symbols(d.type); }))
+        .attr("d", d3.symbol().type(function(d) { return symbols(d.type); }).size(120))
         .attr("data-bs-toggle", "tooltip")
         .attr("data-tippy-content", function(d) {
             return "<b>"+ d.id + "</b><br/>" + d.description;
@@ -76,36 +102,38 @@ d3.json("newdata.json").then(function(graph) {
         .attr("stroke-width", 3)
         .attr("stroke", function(d){
             if (d.contact_type == "Key Contact") {
-                return "goldenrod";
+                return "#278BD2";
             } else {
                 return "none";
             }
         });
 
-    node.on("mouseover", function(d){
-        link.style("stroke-width", function(l){
-            if (d === l.source || d === l.target) {
-                return 4;
-            } else {
-                return 1;
-            } 
-        });
-    });
+    node.on("mouseover", mouseOver(.1))
+        .on("mouseout", mouseOut);
+    // node.on("mouseover", function(d){
+    //     link.style("stroke-width", function(l){
+    //         if (d === l.source || d === l.target) {
+    //             return 4;
+    //         } else {
+    //             return 1;
+    //         } 
+    //     });
+    // });
 
-    node.on("mouseout", function(){
-        link.style("stroke-width", 1);
-    });
+    // node.on("mouseout", function(){
+    //     link.style("stroke-width", 1);
+    // });
 
     var simulation = d3.forceSimulation(graph.nodes)
-    .force("charge", d3.forceCollide().radius(15))
+    .force("charge", d3.forceCollide().radius(20))
     .force("link", d3.forceLink(graph.links).id(d => d.id).strength(0))
     .force("r", d3.forceRadial(function(d) {
-        return d.proximity * 80;
-    }).strength(1))
+        return d.proximity * 100;
+    }).strength(.5))
     .on("tick", ticked);
     // simulation.stop();
-    // simulation.force("link")
-    //     .links(graph.links);
+    simulation.force("link")
+        .links(graph.links);
 
     var labels = node.append("text")
       .text(function(d) {
@@ -115,21 +143,63 @@ d3.json("newdata.json").then(function(graph) {
       .attr('y', 3)
       .attr("opacity", .5);
 
-    tippy('[data-tippy-content]', {
-    allowHTML: true
-});
+    // build a dictionary of nodes that are linked
+    var linkedByIndex = {};
+    links.forEach(function(d) {
+        linkedByIndex[d.source.index + "," + d.target.index] = 1;
+    });
+
+    // check the dictionary to see if nodes are linked
+    function isConnected(a, b) {
+        return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
+    }
+
+    // fade nodes on hover
+    function mouseOver(opacity) {
+        return function(d, i) {
+            // check all other nodes to see if they're connected
+            // to this one. if so, keep the opacity at 1, otherwise
+            // fade
+            
+            node.style("stroke-opacity", function(o) {
+                thisOpacity = isConnected(i, o) ? 1 : opacity;
+                return thisOpacity;
+            });
+            node.style("fill-opacity", function(o){
+                thisOpacity = isConnected(i, o) ? 1 : opacity;
+                return thisOpacity;
+            })
+            // also style link accordingly
+            link.style("stroke-opacity", function(o) {
+                return o.source === i || o.target === i ? 1 : opacity;
+            });
+        };
+    }
+
+    function mouseOut() {
+        node.style("stroke-opacity", 1);
+        node.style("fill-opacity", 1);
+        link.style("stroke-opacity", 1);
+        link.style("stroke", "#333");
+    }
+
+//     tippy('[data-tippy-content]', {
+//     allowHTML: true
+// });
 
 svg.append("g")
-    .attr("class", "legendOrdinal")
-    .attr("transform", "translate(-450,140)");
+    .attr("class", "legendSymbol")
+    .attr("transform", "translate(-450,180)");
 
-var legendOrdinal = d3.legendColor()
-.shape("path", d3.symbol().type(d3.symbolCircle).size(100)())
-.shapePadding(10)
-.scale(pointScale);
+var legendPath = d3.legendSymbol()
+.scale(symbolScale)
+.orient("vertical")
+.labelWrap(30)
+.title("LEGEND")
+.on("cellclick", function(d){ console.log(d)});
 
-svg.select(".legendOrdinal")
-.call(legendOrdinal);
+svg.select(".legendSymbol")
+  .call(legendPath);
 
 function ticked() {
     link
@@ -143,3 +213,15 @@ function ticked() {
         });
 }
 });
+
+function handleZoom(e) {
+    d3.select('svg g')
+      .attr('transform', e.transform);
+  }
+
+function initZoom() {
+d3.select('svg')
+    .call(zoom);
+}
+
+initZoom();
